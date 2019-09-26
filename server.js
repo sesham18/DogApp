c106e8f02b321555792f6e516bb632e484fad773
Sesha Machiraju <sesha.machiraju@avanade.com>
2019-09-26 14:13:52 -0700

    added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const dogRouter = require('./router'); 
+const morgan = require('morgan');
 
 app.use(morgan('common')); 
 app.use(express.static('public'));
