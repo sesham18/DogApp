23e2db0fb03951750cedf51ecc58bb3cf02d556a
Sesha Machiraju <sesha.machiraju@avanade.com>
2019-10-17 12:25:30 -0700

    connected db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                      'mongodb://localhost/doggone';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-                      'mongodb://localhost/doggone-test';
+                      'mongodb://localhost/doggone-app';
+//exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+                      //'mongodb://localhost/doggone-test';
 exports.PORT = process.env.PORT || 8080;
