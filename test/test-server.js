0bff7093e6437b140401f32ddd3ad12a4027c5a1
Sesha Machiraju <sesha.machiraju@avanade.com>
2019-10-17 12:37:48 -0700

    changed int tests

--- test/test-server.js
+++ test/test-server.js
@@ -2,19 +2,22 @@
 
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const app  = require("../server");
+//const app  = require("../server");
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe("index page", function() {
-    it ("exists", function() {
-        return(chai)
-            .request(app)
-            .get("/")
-            .then(function(res){
-                expect(res).to.have.status(200);
-            });
-    });
-})
 
+// import isEqual.js
+const isEqual = require('../isEqual');
+
+// "describe" is used to declare the entity to be tested, and a callback function that sets up the tests
+describe('isEqual', function() {
+    // indicate the behavior to be tested
+    // typically the "it" statement will also include a callback that provides the test
+    it('should give right answers for equal and unequal inputs', function(){
+        expect(isEqual(1, 1)).to.be.true;
+    });
+        
+  }
+);
