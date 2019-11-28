81ab2fec4a91c9d9ac11ee2902ecd22662a1c40c
sesham18 <seshasirimachiraju@gmail.com>
2019-11-28 15:29:46 -0800

    Second important change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser'); 
 const app = express();
 mongoose.Promise = global.Promise;
-const a = 9;
+
 //app.use(express.json());
 
 const MongoClient = require("mongodb").MongoClient;
